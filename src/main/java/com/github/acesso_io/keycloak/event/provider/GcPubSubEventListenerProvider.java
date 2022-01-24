2dac0fbbcc04570b54149cf32fb0bc6e17aae202
jensbn <jens.nilsson@inderes.fi>
2022-01-24 15:26:53 +0200

    Add support for gcloud pubsub emulator.

--- src/main/java/com/github/acesso_io/keycloak/event/provider/GcPubSubEventListenerProvider.java
+++ src/main/java/com/github/acesso_io/keycloak/event/provider/GcPubSubEventListenerProvider.java
@@ -3,9 +3,19 @@ package com.github.acesso_io.keycloak.event.provider;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.admin.AdminEvent;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
 import org.jboss.logging.Logger;
 import com.google.api.core.ApiFuture;
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.grpc.GrpcTransportChannel;
+import com.google.api.gax.rpc.FixedTransportChannelProvider;
+import com.google.api.gax.rpc.TransportChannelProvider;
 import com.google.cloud.pubsub.v1.Publisher;
+import com.google.cloud.pubsub.v1.Publisher.Builder;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.TopicName;
@@ -68,23 +78,42 @@ public class GcPubSubEventListenerProvider implements EventListenerProvider {
 
 		Publisher publisher = null;
 
+		// See: https://cloud.google.com/pubsub/docs/emulator#pubsub-emulator-java
+		String emulatorHostport = System.getenv("PUBSUB_EMULATOR_HOST");
+		ManagedChannel channel = null;
+
+		if (emulatorHostport != null) {
+			channel = ManagedChannelBuilder.forTarget(emulatorHostport).usePlaintext().build();
+		}
 		try {
-			// Create a publisher instance with default settings bound to the topic
-			publisher = Publisher.newBuilder(topicName).build();
+			Builder publisherBuilder = Publisher.newBuilder(topicName);
+
+			if (emulatorHostport != null) {
+				TransportChannelProvider channelProvider =
+				FixedTransportChannelProvider.create(GrpcTransportChannel.create(channel));
+				CredentialsProvider credentialsProvider = NoCredentialsProvider.create();
+				publisherBuilder.setChannelProvider(channelProvider);
+				publisherBuilder.setCredentialsProvider(credentialsProvider);
+			}
+
+			publisher = publisherBuilder.build();
 
 			String message = messageString;
 			ByteString data = ByteString.copyFromUtf8(message);
 			PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).putAllAttributes(attributes).build();
 
-			// Once published, returns a server-assigned message id (unique within the
-			// topic)
+			// Once published, returns a server-assigned message id (unique within the topic)
 			ApiFuture<String> messageIdFuture = publisher.publish(pubsubMessage);
 			String messageId = messageIdFuture.get();
 			logger.info("Published message ID: " + messageId);
 		} catch (IOException | ExecutionException | InterruptedException ex) {
 			logger.error("keycloak-to-gcpubsub ERROR sending message: " + attributes, ex);
-		} finally {
-			if (publisher != null) {
+		}
+		finally {
+			if (emulatorHostport != null) {
+				channel.shutdown();
+			}
+			else if (publisher != null) {
 				try {
 					// When finished with the publisher, shutdown to free up resources.
 					publisher.shutdown();
